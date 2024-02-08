e5db6113b288cab10af1b2928541e5af2a226aef
Chirayu Khandelwal <itschirayu@gmail.com>
2024-02-08 11:33:16 +0530

    Basket API Initial Commit

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,31 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetSection("CacheSettings:ConnectionString").Value;
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
