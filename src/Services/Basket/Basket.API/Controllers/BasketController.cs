c25e6e466d3756adf4a75a9c9cb61de94c9fed0b
Chirayu Khandelwal <itschirayu@gmail.com>
2024-02-16 12:24:02 +0530

    Communicatiion between Discount.Grpc and Basked.API using grpc and protobuff using connected services.

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,10 +11,12 @@ namespace Basket.API.Controllers
     public class BasketController: ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcServices _discountGrpcServices;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcServices discountGrpcServices)
         {
-            _repository = repository;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _discountGrpcServices = discountGrpcServices ?? throw new ArgumentNullException(nameof(discountGrpcServices));
         }
 
         [HttpGet("{userName}", Name = "GetBasket")]
@@ -30,6 +33,11 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody]ShoppingCart basket)
         {
+            foreach (var item in basket.Items) {
+                var coupon = await _discountGrpcServices.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount; 
+            }
+
             return Ok(await _repository.UpdateBasket(basket));
         }
 
---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcServices.cs
@@ -0,0 +1,19 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcServices
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcServices(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            return await _discountProtoService.GetDiscountAsync(new GetDiscountRequest { ProductName = productName });
+        }
+    }
+}
