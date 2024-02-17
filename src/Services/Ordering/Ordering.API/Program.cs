0bf48ce0ebc03c29e5a1c67d418acec4ee114544
Chirayu Khandelwal <itschirayu@gmail.com>
2024-02-17 14:33:42 +0530

    Registering Application and Infrastructure servies to API program.cs

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,6 @@
-using Ordering.Application.Contracts.Persistence;
-using Ordering.Infrastructure.Repositories;
+using Microsoft.Extensions.Configuration;
+using Ordering.Infrastructure;
+using Ordering.Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
